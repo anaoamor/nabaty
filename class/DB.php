afb876df92eee3705582d13bc280b22344b4f971
anaoamor <moriza74@gmail.com>
2025-06-12 14:33:43 +0700

    Updated DB query, mysql server account
    Added inner join query and multiple join query

--- class/DB.php
+++ class/DB.php
@@ -4,8 +4,8 @@ class DB{
   // Property untuk koneksi ke database mysql
   private $_host = '127.0.0.1';
   private $_dbname = 'nabaty';
-  private $_username = '';
-  private $_password = '';
+  private $_username = 'moriza';
+  private $_password = 'pororo0857';
 
   // Property internal dari class DB
   private static $_instance = null;
@@ -159,6 +159,47 @@ class DB{
     return $this->getLeftJoinTwoTables($tableName1, $tableName2, $columnJoin, $queryLike, [$search]);
   }
 
+  //Method to get a list of inner join data
+  public function getInnerJoinTwoTables($tableName1, $tableName2, $columnJoin, $condition = "", $bindValue = []){
+    $query = "SELECT {$this->_columnName} FROM {$tableName1} INNER JOIN {$tableName2} ON {$tableName1}.{$columnJoin} = {$tableName2}.{$columnJoin} {$condition} {$this->_orderBy}";
+    $_columnName = "*";
+    $_orderBy = "";
+    return $this->getQuery($query, $bindValue);
+  }
+
+  //Method to select two table with where conditions
+  public function getInnerJoinTwoTablesWhere($tableName1, $tableName2, $columnJoin, $condition) {
+    $queryWhere = "WHERE {$condition[0]} {$condition[1]} ?";
+    return $this->getInnerJoinTwoTables($tableName1, $tableName2, $columnJoin, $queryWhere, [$condition[2]]);
+  }
+
+  //Method to select two table with multiple where conditions
+  public function getInnerJoinTwoTablesWhereConditionAnd ($tableName1, $tableName2, $columnJoin, $conditions){
+    $conditionQuery = "WHERE ";
+    foreach($conditions as $condition) {
+      $conditionQuery .= " {$condition[0]} {$condition[1]} ? AND";
+      $conditionValue[] = $condition[2];
+    }
+    $conditionQuery = substr($conditionQuery, 0, -3);
+    return $this->getInnerJoinTwoTables($tableName1, $tableName2, $columnJoin, $conditionQuery, $conditionValue);
+  } 
+
+  //Method to select from two tables with 'where' condition, but only first line
+  public function getInnerJoinTwoTablesWhereOnce($tableName1, $tableName2, $columnJoin, $condition) {
+    $result = $this->getInnerJoinTwoTablesWhere($tableName1, $tableName2, $columnJoin, $condition);
+    if(!empty($result)){
+      return $result[0];
+    } else {
+      return false;
+    }
+  }
+
+  //Method to select from two tables based on user query search (LIKE query)
+  public function getInnerJoinTwoTablesLike($tableName1, $tableName2, $columnJoin, $likeColumn, $search) {
+    $likeQuery = "WHERE {$likeColumn} LIKE ? ";
+    return $this->getInnerJoinTwoTables($tableName1,$tableName2, $columnJoin, $likeQuery, [$search]);
+  }
+
   // Method untuk check nilai unik, akan berguna untuk form
   public function check($tableName, $columnName, $dataValues){
     $query = "SELECT {$columnName} FROM {$tableName} WHERE {$columnName} = ? ";
@@ -215,6 +256,22 @@ class DB{
     return $this->_count;
   }
 
+  //Method to get data from multiple tables(3 or more)
+  public function getMultipleTable($joinArray, $conditions = "", $bindValue  = []){
+    $query = "SELECT {$this->_columnName} FROM {$joinArray[0][1][0]} {$joinArray[0][0]} {$joinArray[0][1][1]} ON {$joinArray[0][1][0]}.{$joinArray[0][2]} = {$joinArray[0][1][1]}.{$joinArray[0][2]}";
+    for($i = 1; $i<count($joinArray); $i++){
+      $query .= " {$joinArray[$i][0]} {$joinArray[$i][1][1]} ON {$joinArray[$i][1][0]}.{$joinArray[$i][2]} = {$joinArray[$i][1][1]}.{$joinArray[$i][2]}";
+    }
+    $this->_columnName = "*";
+    $this->_orderBy = "";
+    $this->_limit = "";
+    return $this->getQuery($query, $bindValue);
+    return $query;
+
+    // $query = "SELECT {$this->_columnName} FROM {$tableName} {$condition} {$this->_orderBy} {$this->_limit}";
+    
+  }
+  
   // Method untuk menghapus data tabel (query DELETE)
   public function delete($tableName, $condition){
     $query = "DELETE FROM {$tableName} WHERE {$condition[0]} {$condition[1]} ? ";
